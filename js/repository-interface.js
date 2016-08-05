7ded522a6ef901bc2e429e7d8862447a17d77001
Joel Delight <thefencingflutist@gmail.com>
2016-08-05 11:52:50 -0700

    adjust the display of information about repositories and create links to the repositories out of the repository names.

--- js/repository-interface.js
+++ js/repository-interface.js
@@ -6,9 +6,9 @@ var displayAllRepositories = function(array) {
   array.forEach(function(item) {
     if(item.description === null || item.description === "")
     {
-      $(".results").append("<li>" + item.name + " | No description was given for this repository.");
+      $(".results").append("<li class='project'><a href='" + item.html_url + "'>" + item.name + "</a></li><ul><li>No description was given for this repository.</li><li>Created on- " + item.created_at.slice(0,10) + "</li></ul>");
     } else {
-      $(".results").append("<li>" + item.name + " | " + item.description);
+      $(".results").append("<li class='project'><a href='" + item.html_url + "'>" + item.name + "</a></li><ul><li>" + item.description + "</li><li>Created on- " + item.created_at.slice(0,10) + "</li></ul>");
     }
   });
 };
