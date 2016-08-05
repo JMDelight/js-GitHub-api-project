4c5ab9a01d1d23a4933d52c1cdb4a858c58a08a2
Joel Delight <thefencingflutist@gmail.com>
2016-08-05 10:20:24 -0700

    reduce the size of the users profile picture and display a message when there is no project description for a GitHub project.

--- js/repository-interface.js
+++ js/repository-interface.js
@@ -4,18 +4,18 @@ var gitHubUsersObject = new GitHubUsers();
 var displayAllRepositories = function(array) {
   $(".results").empty();
   array.forEach(function(item) {
-    // if(item.description === null || item.description === "")
-    // {
-    //   $(".results").append("<li>" + item.name + " | No description was given.");
-    // } else {
-    //   $(".results").append("<li>" + item.name + " | " + item.description);
-    // }
+    if(item.description === null || item.description === "")
+    {
+      $(".results").append("<li>" + item.name + " | No description was given for this repository.");
+    } else {
+      $(".results").append("<li>" + item.name + " | " + item.description);
+    }
     console.log(item.name + " | " + item.description);
   });
 };
 
 var displayAvatar = function(response) {
-  $(".avatar").append("<img src=https://avatars.githubusercontent.com/u/" + response.id + " alt='the entered GitHub users profile picture'>");
+  $(".avatar").append("<img src=https://avatars.githubusercontent.com/u/" + response.id + " alt='the entered GitHub users profile picture' width=200>");
 };
 
 $(document).ready(function() {
@@ -27,5 +27,4 @@ $(document).ready(function() {
     gitHubUsersObject.getInfo(nameToLookUp, displayAvatar);
 
   });
-  gitHubUsersObject.getRepos("JMDelight");
 });
